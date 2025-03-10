2f2b63d7c32a31b8c224d824296b1d30385afc86
Ilya Evdokimov <evdokimov.ilya@gmail.com>
2025-03-10 22:47:43 +0100

    re-arranged removal

--- btc_update.py
+++ btc_update.py
@@ -793,18 +793,13 @@ if __name__ == "__main__":
                             emojis=emoji,
                             tgs_sticker=None,
                         )
-                        sleep(30)
+                    for s in old_pack.stickers:
+                        if s.emoji == emoji:
+                            req = bot.delete_sticker_from_set(s.file_id)
+                    sleep(30)
             except Exception as e:
                 sleep(30)
 
-        for s in old_pack.stickers:
-            if s.emoji == emoji:
-                try:
-                    req = bot.delete_sticker_from_set(s.file_id)
-                except:
-                    sleep(30)
-                finally:
-                    req = bot.delete_sticker_from_set(s.file_id)
 
     except Exception as e:
         bot.send_message(USER_ID, f"Sticker pack update, exception caught: {e}")
